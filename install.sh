99593664462ccc3e971fbca61fa143a3d2c1e0cc
gapan <gapan@a8afeb8c-277c-4b4a-a4f6-ef4c85adc32b>
2009-11-17 08:52:07 +0000

    Made the cron script executable

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ install -d -m 755 $DESTDIR/etc/xdg/autostart
 install -d -m 755 $DESTDIR/etc/cron.hourly
 install -m 755 src/salix-update-notifier $DESTDIR/usr/bin/
 install -m 644 salix-update-notifier.desktop $DESTDIR/etc/xdg/autostart/
-install -m 644 src/slapt-get-update $DESTDIR/etc/cron.hourly/
+install -m 755 src/slapt-get-update $DESTDIR/etc/cron.hourly/
 
 for i in `ls locale/*.mo|sed "s|locale/\(.*\).mo|\1|"`; do
 	install -d -m 755 $DESTDIR/usr/share/locale/${i}/LC_MESSAGES
