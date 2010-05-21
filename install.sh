c52b2d875d4097080543aea4eb5d8443db61a4e3
gapan <gapan@a8afeb8c-277c-4b4a-a4f6-ef4c85adc32b>
2010-05-21 12:10:36 +0000

    Added the upgrade script to install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,13 @@ install -d -m 755 $DESTDIR/usr/bin/
 install -d -m 755 $DESTDIR/etc/xdg/autostart
 install -d -m 755 $DESTDIR/etc/cron.hourly
 install -d -m 755 $DESTDIR/usr/share/pixmaps
+install -d -m 755 $DESTDIR/usr/libexec
 install -m 755 src/salix-update-notifier $DESTDIR/usr/bin/
 install -m 644 salix-update-notifier.desktop $DESTDIR/etc/xdg/autostart/
 install -m 755 src/slapt-get-update $DESTDIR/etc/cron.hourly/
 install -m 644 src/salix-update-notifier.conf $DESTDIR/etc/
 install -m 644 src/salix-update-notifier.png $DESTDIR/usr/share/pixmaps/
+install -m 755 src/salix-update-notifier-upgrade $DESTDIR/usr/libexec/
 
 for i in `ls po/*.mo|sed "s|po/\(.*\).mo|\1|"`; do
 	install -d -m 755 $DESTDIR/usr/share/locale/${i}/LC_MESSAGES
